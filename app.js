143c7c453366ae051b59d782cef4d48cdd138339
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2024-12-08 00:01:11 +0200

    Loop and display messages

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const app = express();
 const PORT = 3000;
 const path = require("node:path");
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { message: "This is the homepage"});
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 app.listen(PORT, () => {
